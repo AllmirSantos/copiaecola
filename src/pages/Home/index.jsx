ac9158e6e8413f52a6821b0675ea9beaedd6d385
Almir Santos <almir.santos@gok.digital>
2023-10-03 16:34:08 -0300

    update

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import * as S from "./styles";
+
+export function Home() {
+  const [isOpenPopUp, setIsOpenPopUp] = useState(false);
+
+  function onOpenPopUp() {
+    setIsOpenPopUp(!isOpenPopUp);
+  }
+  return (
+    <S.Container>
+      <h1>
+        #Copia e colar
+      </h1>
+      <button
+        onClick={() => {
+          onOpenPopUp(true);
+        }}
+      >
+        Entrar
+      </button>
+      <br />
+      <br />
+      {isOpenPopUp && <text>Hello </text>}
+
+      <ul>
+        <li>Bottoes</li>
+        <li>Efeito click</li>
+        <li>Navegaçao</li>
+        <li>Usar Theme</li>
+        <li>Animaçoes</li>
+        <li>Outros</li>
+      </ul>
+    </S.Container>
+  );
+}
